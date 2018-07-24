f9ab60b0fd3ab0f9a5a36999735602c960ae53d0
wwh <wangwen135@163.com>
2018-07-25 01:50:29 +0800

    常量和工具方法

--- src/main/java/com/wwh/whwtools/generator/GeneratorConstant.java
+++ src/main/java/com/wwh/whwtools/generator/GeneratorConstant.java
@@ -1,5 +1,8 @@
 package com.wwh.whwtools.generator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <pre>
  * 代码生成器常量类
@@ -29,4 +32,56 @@ public class GeneratorConstant {
      * </pre>
      */
     public static final String DEFAULT_TABLE_COLUMN_NAME_SEPARATOR = "_";
+
+    /**
+     * <pre>
+     * 数据库类型
+     * mysql
+     * </pre>
+     */
+    public static enum DBType {
+        MYSQL
+    }
+
+    /**
+     * <pre>
+     * 数据库类型到java类型映射
+     * 应该需要支持从界面上进行配置
+     * </pre>
+     */
+    public static final Map<String, String> DB_TO_JAVA_MAPPING = new HashMap<String, String>();
+    static {
+        DB_TO_JAVA_MAPPING.put("VARCHAR", "java.lang.String");
+        DB_TO_JAVA_MAPPING.put("CHAR", "java.lang.String");
+        DB_TO_JAVA_MAPPING.put("BIGINT", "java.lang.Long");
+        DB_TO_JAVA_MAPPING.put("INT", "java.lang.Integer");
+        DB_TO_JAVA_MAPPING.put("TINYINT", "java.lang.Integer");
+        DB_TO_JAVA_MAPPING.put("SMALLINT", "java.lang.Integer");
+        DB_TO_JAVA_MAPPING.put("MEDIUMINT", "java.lang.Integer");
+        DB_TO_JAVA_MAPPING.put("DECIMAL", "java.math.BigDecimal");
+        DB_TO_JAVA_MAPPING.put("DATETIME", "java.util.Date");
+        DB_TO_JAVA_MAPPING.put("DATE", "java.util.Date");
+        DB_TO_JAVA_MAPPING.put("TIMESTAMP", "java.util.Date");
+        DB_TO_JAVA_MAPPING.put("BIT", "java.lang.Boolean");
+        DB_TO_JAVA_MAPPING.put("FLOAT", "java.lang.Float");
+        DB_TO_JAVA_MAPPING.put("DOUBLE", "java.lang.Double");
+        DB_TO_JAVA_MAPPING.put("BLOB", "java.lang.byte[]");
+        DB_TO_JAVA_MAPPING.put("TEXT", "java.lang.String");
+        DB_TO_JAVA_MAPPING.put("LONGTEXT", "java.lang.String");
+        DB_TO_JAVA_MAPPING.put("MEDIUMTEXT", "java.lang.String");
+    }
+
+    /**
+     * <pre>
+     * 数据库类型到Mybatis jdbcType 映射
+     * 
+     * </pre>
+     */
+    public static final Map<String, String> DB_TO_MYBATIS_MAPPING = new HashMap<String, String>();
+    static {
+        DB_TO_MYBATIS_MAPPING.put("INT", "INTEGER");
+        DB_TO_MYBATIS_MAPPING.put("DATETIME", "TIMESTAMP");
+        DB_TO_MYBATIS_MAPPING.put("DATETIME", "TIMESTAMP");
+        DB_TO_MYBATIS_MAPPING.put("TEXT", "VARCHAR");
+    }
 }
